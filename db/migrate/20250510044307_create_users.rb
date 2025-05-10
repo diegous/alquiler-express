ad9212870b132166411e360b43ad8835ca18e977
diegous <munoa.d@gmail.com>
2025-05-10 02:24:45 -0300

    Add attributes to create_users migration file

--- db/migrate/20250510044307_create_users.rb
+++ db/migrate/20250510044307_create_users.rb
@@ -3,6 +3,12 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :dni, index: { unique: true }
+      t.string :first_name
+      t.string :last_name
+      t.date :dob
+      t.integer :gender
+      t.string :phone
 
       t.timestamps
     end
