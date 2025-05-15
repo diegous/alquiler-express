865099ba134bd05d535decaab1a4084a898db747
diegous <munoa.d@gmail.com>
2025-05-15 13:18:23 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_12_232303) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_13_143514) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_12_232303) do
     t.float "surface"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "type"
+    t.float "price"
+    t.text "description"
   end
 
   create_table "rentals", force: :cascade do |t|
