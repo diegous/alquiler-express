e99c31a1f3de640d857a15322a592cb9e820a43e
diegous <munoa.d@gmail.com>
2025-05-15 13:20:16 -0300

    Add LivingPropertie edit page

--- app/controllers/living_properties_controller.rb
+++ app/controllers/living_properties_controller.rb
@@ -4,4 +4,25 @@ class LivingPropertiesController < ApplicationController
   def show
     @property = LivingProperty.find(params[:id])
   end
+
+  def edit
+    @property = LivingProperty.find(params[:id])
+  end
+
+  def update
+    @property = LivingProperty.find(params[:id])
+
+    if @property.update(living_property_params)
+      redirect_to living_property_path(@property)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def living_property_params
+    params.require(:living_property)
+          .permit(:name, :bedrooms, :guest_capacity, :price, :city, :pictures, :description)
+  end
 end
