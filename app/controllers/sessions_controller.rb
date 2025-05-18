7dcc4fb6d7df7c553e4b9e5bc15d6ef1877d33dd
diegous <munoa.d@gmail.com>
2025-05-18 00:04:55 -0300

    Fixed logout not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :redirect_if_logged_in
+  before_action :redirect_if_logged_in, only: %i[ new create ]
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
