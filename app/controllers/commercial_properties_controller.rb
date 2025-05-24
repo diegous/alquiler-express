1abb427276a1d41a00215b30b1dd1756e83997c0
Tomás Campanella <tomyecamp@gmail.com>
2025-05-24 16:47:50 -0300

    Agrego filtros de locales

--- app/controllers/commercial_properties_controller.rb
+++ app/controllers/commercial_properties_controller.rb
@@ -4,6 +4,20 @@ class CommercialPropertiesController < ApplicationController
 
   def index
     @properties = CommercialProperty.all
+
+    if params[:city].present?
+      @properties = @properties.where("city like ?", "%#{params[:city].strip}%")
+    end
+
+    if params[:min_m2].present?
+      min_m2 = params[:min_m2].to_f
+      @properties = @properties.where("width * length >= ?", min_m2)
+    end
+
+    if params[:max_m2].present?
+      max_m2 = params[:max_m2].to_f
+      @properties = @properties.where("width * length <= ?", max_m2)
+    end
   end
 
   def show
