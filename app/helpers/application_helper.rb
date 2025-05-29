1dd77a913cd54905d1fb995a5646cc7fb29548fd
diegous <munoa.d@gmail.com>
2025-05-29 11:24:20 -0300

    Rubocop fix: cambio comillas simples por dobles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,16 @@ module ApplicationHelper
 
   def flash_colors(type)
     case type
-    when 'success'
-      'bg-green-50  border-green-400  text-green-700'
-    when 'error'
-      'bg-red-50    border-red-400    text-red-700'
-    when 'warning'
-      'bg-yellow-50 border-yellow-400 text-yellow-700'
-    when 'notice'
-      'bg-blue-50   border-blue-400   text-blue-700'
+    when "success"
+      "bg-green-50  border-green-400  text-green-700"
+    when "error"
+      "bg-red-50    border-red-400    text-red-700"
+    when "warning"
+      "bg-yellow-50 border-yellow-400 text-yellow-700"
+    when "notice"
+      "bg-blue-50   border-blue-400   text-blue-700"
     else
-      'bg-gray-50   border-gray-400   text-gray-700'
+      "bg-gray-50   border-gray-400   text-gray-700"
     end
   end
 end
