a5cd58c4327cfdd9a96129fe4bf00bf69bd4ab02
diegous <munoa.d@gmail.com>
2025-07-09 23:07:20 -0300

    Fix: no redirigir al visitante a iniciar sesión al acceder al root path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  allow_unauthenticated_access only: %i[ redirect_by_role ]
+
   def redirect_by_role
     if Current.user&.admin?
       redirect_to admin_living_properties_path
